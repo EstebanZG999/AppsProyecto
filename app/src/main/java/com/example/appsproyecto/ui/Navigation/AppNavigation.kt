007076d5267e313aabc49a8e0645beb124d06840
EstebanZG999 <zam22119@uvg.edu.gt>
2023-10-19 17:13:13 -0600

    MVVM Architecture and starting the navigation and screens

--- app/src/main/java/com/example/appsproyecto/ui/Navigation/AppNavigation.kt
+++ app/src/main/java/com/example/appsproyecto/ui/Navigation/AppNavigation.kt
@@ -1,2 +1,14 @@
 package com.example.appsproyecto.ui.Navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+    NavHost(navController= navController, startDestination= TabScreens.MainScreen.route){
+
+    }
+}
--- app/src/main/java/com/example/appsproyecto/ui/Navigation/TabScreens.kt
+++ app/src/main/java/com/example/appsproyecto/ui/Navigation/TabScreens.kt
@@ -1,4 +1,7 @@
 package com.example.appsproyecto.ui.Navigation
 
-class TabScreens {
+sealed class TabScreens(val route: String){
+    object MainScreen: TabScreens("main")
+    object BookPageScreen: TabScreens("bookpage")
+    object ChatScreen: TabScreens("chat")
 }
