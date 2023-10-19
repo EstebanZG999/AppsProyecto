4ca773e35da925c50e8f758a9a6b7080a9f919de
Edwin Ortega <andyortegakou@yahoo.com.mx>
2023-10-19 17:35:24 -0600

    Prueba de Commit

--- app/src/main/java/com/example/appsproyecto/ui/Screens/BookPage/BookPageScreen.kt
+++ app/src/main/java/com/example/appsproyecto/ui/Screens/BookPage/BookPageScreen.kt
@@ -1,2 +1,43 @@
 package com.example.appsproyecto.ui.Screens.BookPage
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appsproyecto.ui.theme.AppsProyectoTheme
+
+class BookPageScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppsProyectoTheme {
+                pageContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun bookPageScreen(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        pageContent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    AppsProyectoTheme {
+        pageContent()
+    }
+}
+
+@Composable
+fun pageContent(){
+
+}
