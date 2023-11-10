18babe78b9b0082f99ab5944f4f6895e030095b3
MENENDEZGUERRA <menendezguerrajuan@gmail.com>
2023-11-09 20:43:45 -0600

    More google Sign In logic (View Model)

---
+++ app/src/main/java/com/example/appsproyecto/presentation/sign_in/SignInState.kt
@@ -0,0 +1,8 @@
+package com.example.appsproyecto.presentation.sign_in
+
+import java.lang.Error
+
+data class SignInState(
+    val isSignInSuccessful: Boolean = false,
+    val signInError: String? = null
+)
