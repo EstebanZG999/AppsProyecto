659522b4811fe266670c50f8f4583bd45918841d
EstebanZG999 <zam22119@uvg.edu.gt>
2023-11-15 20:30:49 -0600

    Log In V2 Branch Fixed gradle

--- app/src/main/java/com/example/appsproyecto/presentation/sign_in/SignInState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.appsproyecto.presentation.sign_in
-
-import java.lang.Error
-
-data class SignInState(
-    val isSignInSuccessful: Boolean = false,
-    val signInError: String? = null
-)
